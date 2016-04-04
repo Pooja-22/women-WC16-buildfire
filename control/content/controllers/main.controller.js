017300f699289ca1919b5584c0cdd59380596ec0
Pooja-22 <pooja.garg@tothenew.com>
2016-04-04 16:32:04 +0530

    Test Sms

--- control/content/controllers/main.controller.js
+++ control/content/controllers/main.controller.js
@@ -3,40 +3,95 @@
  */
 
 angular.module('women-WC16')
-    .controller('MainCtrl', ['$scope','womenService', function ($scope,womenService) {
 
-        $scope.allMatches = function(){
-            $scope.selected1 = true;
-          womenService.setAllMatches($scope.selected1);
+    .controller('MainCtrl', ['$scope', function ($scope) {
+
+        $scope.flag = {
+            allMatches: "",
+            resultsByCountryCode: "",
+            teamResults: "",
+            groupResults: "",
+            teams: ""
         };
-        $scope.removeAllMatches = function(){
-            $scope.selected1 = false;
-            womenService.setAllMatches($scope.selected1);
+
+        /**
+         * Get value of the Flags
+         * @param data
+         */
+
+        $scope.getFlags = function (data) {
+            $scope.flag = data;
+            if (!$scope.$$phase) {
+                $scope.$digest();
+            }
         };
 
-        $scope.checking = function(){
-            console.log("checking",womenService.getAllMatches());
-        }
+        /**
+         * Save Checkbox Flag in datastore
+         * @param flag
+         */
 
+        $scope.saveFlags = function () {
+            flag = $scope.flag;
+            buildfire.datastore.save(flag, "Flag", function (err, data) {
+                if (err)
+                    console.log('there was a problem saving your data');
+                else
+                    console.log('your data has saved successfully');
+            });
+        };
 
+        /**
+         * Get Value of flag
+         */
 
-        /// create a new instance of the buildfire carousel editor
+        buildfire.datastore.get('Flag', function (err, data) {
+            if (err)
+                console.log('there was a problem retrieving your data');
+            else{
+                $scope.getFlags(data.data);
+            }
+
+        });
+
+        /**
+         *  create a new instance of the buildfire carousel editor
+         * @type {buildfire.components.carousel.editor}
+         */
+
         var editor = new buildfire.components.carousel.editor("#carousel");
-        /// handle the loading
+
+        /**
+         * Handle the loading
+         * @param carouselItems
+         */
+
         function loadItems(carouselItems) {
-            // create an instance and pass it the items if you don't have items yet just pass []
+
+            /**
+             * create an instance and pass it the items if you don't have items yet just pass []
+             */
+
             editor.loadItems(carouselItems);
         }
-        /// call buildfire datastore to see if there are any previously saved items
+
+        /**
+         * call buildfire datastore to see if there are any previously saved items
+         */
+
         buildfire.datastore.get(function (err, obj) {
             if (err)
                 alert('error');
             else
                 loadItems(obj.data.carouselItems)
         });
-        /// save any changes in items
+
+        /**
+         * Save any changes in item
+         * @param items
+         */
+
         function save(items) {
-            console.log('saving...');
             buildfire.datastore.save({carouselItems: items}, function (e) {
                 if (e)
                     alert("error");
@@ -45,19 +100,41 @@ angular.module('women-WC16')
             });
         }
 
-        // this method will be called when a new item added to the list
+        /**
+         *  this method will be called when a new item added to the list
+         * @param items
+         */
+
         editor.onAddItems = function (items) {
             save(editor.items);
         };
-        // this method will be called when an item deleted from the list
+
+        /**
+         * this method will be called when an item deleted from the list
+         * @param item
+         * @param index
+         */
+
         editor.onDeleteItem = function (item, index) {
             save(editor.items);
         };
-        // this method will be called when you edit item details
+
+        /**
+         * this method will be called when you edit item details
+         * @param item
+         */
+
         editor.onItemChange = function (item) {
             save(editor.items);
         };
-        // this method will be called when you change the order of items
+
+        /**
+         * This method will be called when you change the order of items
+         * @param item
+         * @param oldIndex
+         * @param newIndex
+         */
+
         editor.onOrderChange = function (item, oldIndex, newIndex) {
             save(editor.items);
         };
--- widget/controllers/widget.controller.js
+++ widget/controllers/widget.controller.js
@@ -4,61 +4,169 @@
 
 angular.module('women-WC16-Widget')
 
-    .controller('WidgetCtrl', ['$scope', 'teamResult', 'allMatches', 'resultByCode', 'groupResults', 'teams','womenService', function ($scope, teamResult, allMatches, resultByCode, groupResults, teams, womenService) {
+    .controller('WidgetCtrl',function($scope){
 
-        $scope.dataValue = '';
-        $scope.dataValueChanged = function () {
-            switch ($scope.dataValue) {
-                case 'A':
-                    teamResult.getData(function (data) {
-                        $scope.result = data;
-                    });
-                    break;
-                case 'B':
-                    allMatches.getData(function (data) {
-                        $scope.result = data;
-                    });
-                    break;
-                case 'C':
-                    resultByCode.getData(function (data) {
-                        $scope.result = data;
-                    });
-                    break;
-                case 'D':
-                    groupResults.getData(function (data) {
-                        $scope.result = data;
-                    });
-                    break;
-                case 'E':
-                    teams.getData(function (data) {
-                        $scope.result = data;
-                    });
-                    break;
-            }
-
+        var sendSMS = {
+            title: "Send SMS",
+            subject : "action Item SMS Example",
+            body : "We are testing action Item send SMS",
+            phoneNumber: "+918439703002",
+            action: "sendSMS"
         };
 
+        buildfire.datastore.save (sendSMS, 'actionItems' ,function(err,data){
+            if(err){
+                console.log("error")
+            }
+            else{
+                console.log('resultiiiiiiiiiiii',data)
+            }
+        });
 
-        ///create new instance of buildfire carousel viewer
-        var view = new buildfire.components.carousel.view("#carousel", []);
+        buildfire.datastore.get('actionItems',function(err,data){
+            $scope.actionItem = data.data;
+            console.log("#############################33", data.data);
+        });
 
-        /// load items
-        function loadItems(carouselItems) {
-            // create an instance and pass it the items if you don't have items yet just pass []
-            view.loadItems(carouselItems);
+
+        $scope.send = function(){
+            console.log("!!!!!!!!!!!!!!!!!!!!!!!!!1")
+            buildfire.actionItems.execute($scope.actionItem ,function(err,result){
+                console.log("qwertyuio")
+                if(err){
+                    console.log("errorrrrrrrrrrrr",err);
+                }
+                else{
+                    console.log('resultttttttttt',result);
+                }
+
+            })
         }
 
-        /// load any previously saved items
-        buildfire.datastore.get(function (err, obj) {
-            if (err)
-                alert('error');
-            else
-                loadItems(obj.data.carouselItems)
-        });
+        //buildfire.actionItems.showDialog ('sendEmail' , function(err,data){
+        //    console.log("yooooooooooooooooooooooooooooooooooooo")
+        //   console.log(data);
+        //});
 
-        /// handle any updates by reloading
-        buildfire.datastore.onUpdate(function (e) {
-            loadItems(e.obj.carouselItems);
-        });
 
-    }]);
+        console.log("wooooooooooooooooooo")
+    });
+
+    //.controller('WidgetCtrl', ['$scope', 'teamResult', 'allMatches', 'resultByCode', 'groupResults', 'teams', function ($scope, teamResult, allMatches, resultByCode, groupResults, teams) {
+    //
+    //    $scope.dataValue = '';
+    //
+    //    /**
+    //     * Get Data on the basis of the selection of radio button
+    //     */
+    //
+    //    $scope.dataValueChanged = function () {
+    //        switch ($scope.dataValue) {
+    //            case 'A':
+    //                teamResult.getData(function (data) {
+    //                    $scope.result = data;
+    //                });
+    //                break;
+    //            case 'B':
+    //                allMatches.getData(function (data) {
+    //                    $scope.result = data;
+    //                });
+    //                break;
+    //            case 'C':
+    //                resultByCode.getData(function (data) {
+    //                    $scope.result = data;
+    //                });
+    //                break;
+    //            case 'D':
+    //                groupResults.getData(function (data) {
+    //                    $scope.result = data;
+    //                });
+    //                break;
+    //            case 'E':
+    //                teams.getData(function (data) {
+    //                    $scope.result = data;
+    //                });
+    //                break;
+    //        }
+    //
+    //    };
+    //
+    //    $scope.flag = {
+    //        allMatches: "",
+    //        resultsByCountryCode: "",
+    //        teamResults: "",
+    //        groupResults: "",
+    //        teams: ""
+    //    };
+    //
+    //    /**
+    //     * Get value of the Flags
+    //     * @param data
+    //     */
+    //
+    //    $scope.getFlags = function (data) {
+    //        $scope.flag = data;
+    //        if (!$scope.$$phase) {
+    //            $scope.$digest();
+    //        }
+    //    };
+    //
+    //    /**
+    //     * Get Flags from content using datastore
+    //     * @type {buildfire.components.carousel.view}
+    //     */
+    //
+    //    buildfire.datastore.get('Flag', function (err, data) {
+    //        if (err)
+    //            console.log('there was a problem retrieving your data');
+    //        else{
+    //            $scope.getFlags(data.data);
+    //        }
+    //
+    //    });
+    //
+    //    /**
+    //     * create new instance of buildfire carousel viewer
+    //     * @type {buildfire.components.carousel.view}
+    //     */
+    //
+    //    var view = new buildfire.components.carousel.view("#carousel", []);
+    //
+    //    /**
+    //     * load items
+    //     * @param carouselItems
+    //     */
+    //
+    //    function loadItems(carouselItems) {
+    //
+    //        /**
+    //         *  create an instance and pass it the items if you don't have items yet just pass []
+    //         */
+    //
+    //        view.loadItems(carouselItems);
+    //    }
+    //
+    //    /**
+    //     * load any previously saved items
+    //     */
+    //
+    //    buildfire.datastore.get(function (err, obj) {
+    //        if (err)
+    //            alert('error');
+    //        else
+    //            loadItems(obj.data.carouselItems)
+    //    });
+    //
+    //    /**
+    //     *  handle any updates by reloading
+    //     */
+    //
+    //    buildfire.datastore.onUpdate(function (e) {
+    //        if(e.tag == 'Flag'){
+    //            $scope.getFlags(e.data)
+    //        }
+    //        else
+    //        loadItems(e.obj.carouselItems);
+    //    });
+
+    //}]);
